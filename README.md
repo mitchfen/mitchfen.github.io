979883a535f0cb5f74cd187e5b2982ae7cd07530
Mitchell Fenner <mitchfen@protonmail.com>
2021-03-02 11:38:19 -0500

    Minimize dependencies, restyle
    Using flexbox instead of Bootstrap CSS. No need for jQuery or popper.js

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ![CI/CD](https://github.com/mitchfen/mitchfen.github.io/workflows/CI/CD/badge.svg)
 
-I was originally using the static site generator [Jekyll](https://github.com/jekyll/jekyll) to build my site, but eventually built my own static site with just HTML and Bootstrap CSS. That was a "good enough" solution, but I wanted to learn React, GitHub actions, and Docker.
+I was originally using the static site generator [Jekyll](https://github.com/jekyll/jekyll) to build my site, but eventually built my own static site with just HTML and Bootstrap CSS. That was a "good enough" solution, but I wanted to play around with React, GitHub actions, and Docker.
 
 ### GitHub Actions 🚀
 
@@ -11,4 +11,3 @@ I was originally using the static site generator [Jekyll](https://github.com/jek
 ### Docker 🐳
 
 - I built a Docker image in order to test the website on platforms like Azure and Digital Ocean.
-- The "implementing-backend" branch is where I am implementing a .Net Core 5.0 backend. When I have that finished I will dockerize it and deploy with a cloud provider.
--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,4 @@
-import headShot from "../assets/headshot.jpg";
+//import resume from "../assets/Mitchell-Fenner-resume.pdf";
 
 import {
   SiGithub,
@@ -11,58 +11,108 @@ import {
 const About = () => {
   return (
     <>
-      <div className="row">
-        <div className="col-sm-3">
+      <div className="flex-parent">
+        <div className="left-column">
+          <fieldset>
+            <legend style={{ borderStyle: "round" }}>intro.cob</legend>
+            <code className="pink">IDENTIFICATION DIVISION.</code>
+            <br />
+            <code className="pink">PROGRAM-ID. </code>
+            <code>mitchfen.xyz.</code>
+            <br />
+            <code className="pink">AUTHOR. </code>
+            <code className="yellow">Mitchell Fenner.</code>
+            <br />
+            <code className="pink">PROCEDURE DIVISION.</code>
+            <br />
+            <code className="pink">DISPLAY </code>
+
+            <code>"Welcome to my site! 🤗"</code>
+            <br />
+            <code className="pink">STOP RUN.</code>
+          </fieldset>
           <br />
-          <br />
-          <img src={headShot} width="100%" alt="Me" />
-        </div>
-        <div className="col-sm-9">
-          <p>
-            <br />
-            <br />
-            Welcome! My name is Mitchell Fenner. <br />
-            I'm passionate about automation, and I'm always working on some type
-            of project.
-            <br />
-            <br />
-            <SiGithub />{" "}
+          <fieldset>
+            <legend>contact.txt</legend>
             <a
               href="https://github.com/mitchfen"
               rel="noopener noreferrer"
               target="_blank"
             >
-              mitchfen
-            </a>
-            <br />
-            <SiLinkedin />{" "}
-            <a href="https://www.linkedin.com/in/mitchfen/">mitchfen</a>
-            <br />
-            <SiHackerrank />{" "}
+              <SiGithub className="social" size={44} />
+            </a>{" "}
             <a
-              href="https://hackerrank.com/mitchfen"
+              href="https://www.linkedin.com/in/mitchfen/"
               rel="noopener noreferrer"
               target="_blank"
             >
-              mitchfen
+              <SiLinkedin className="social" size={44} />
             </a>{" "}
-            <br />
-            <SiOrcid />{" "}
-            <a href="https://orcid.org/0000-0002-9684-0447">
-              0000-0002-9684-0447
+            <a
+              href="https://www.hackerrank.com/mitchfen"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <SiHackerrank className="social" size={44} />
+            </a>{" "}
+            <a
+              href="https://orcid.org/0000-0002-9684-0447"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <SiOrcid className="social" size={44} />
+            </a>{" "}
+            <a href="mailto:mitchfen@protonmail.com">
+              <SiMailDotRu className="social" size={44} />
             </a>
             <br />
-            <SiMailDotRu />{" "}
-            <a href="mailto:mitchfen@protonmail.com">mitchfen@protonmail.com</a>
-            <br />
-            <br />
-            I built this site with React and deployed on GitHub pages using
-            GitHub actions.
-            <br />
-            <a href="https://github.com/mitchfen/personal_website">
-              View the code 🡥
-            </a>
-          </p>
+            <small>
+              <a
+                href="https://github.com/mitchfen/personal_website"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                View the code for this site 🡥
+              </a>
+            </small>
+          </fieldset>
+        </div>
+        <br />
+        <div className="right-column">
+          <fieldset>
+            <legend>recent-projects.txt</legend>
+            <p>
+              <p className="pink">This website! 🌐</p>
+              <ul>
+                <li>
+                  Built with React and automated with GitHub Actions. I am more
+                  interested in DevOps than front end development, so I am using
+                  the site as an opportunity to play with CI/CD, Docker, and
+                  cloud hosting platforms.
+                </li>
+              </ul>
+            </p>
+            <p>
+              <p className="pink">Eve Online Price Checker 📡</p>
+              <ul>
+                <li>
+                  Dockerized TypeScript app which uses a REST API to determine
+                  which ore to mine in EVE Online.
+                </li>
+              </ul>
+            </p>
+            <p>
+              <p className="pink">QEMU/KVM Homelab 🧪</p>
+              <ul>
+                <li>
+                  Built my home server running a Ryzen 2700 and Arch Linux. I
+                  setup VMs for pfSense and Windows using VFIO for hardware
+                  passthrough. Also runs pi-hole under docker, Grafana for
+                  monitoring, and samba to serve files.
+                </li>
+              </ul>
+            </p>
+          </fieldset>
         </div>
       </div>
     </>
